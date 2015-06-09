ec67f008324883dfd636088f120381dfa5fd88b2
IlyaGubarev <ilya.gubarev@gmail.com>
2015-06-10 00:03:46 +0400

    application middleware updated

--- source/main/modules/application.js
+++ source/main/modules/application.js
@@ -2,16 +2,34 @@ var express = require('express');
 
 express()
 
+  .use('/resources/static', express.static('./target/resources/static'))
+
   .set('view engine', 'jade')
 
   .set('views', './target/resources/templates')
 
   .get('/resources/templates/:id(*).(htm|html)', function (request, response) {
-    response.render(request.params.id, {});
+    response.render(request.params.id);
+  })
+
+  .get('/resources/models/threads.json', function (request, response) {
+    response.json([
+      {
+        id: 'music',
+        title: 'Music'
+      }
+    ]);
   })
 
   .get('/(*)', function (request, response) {
-    response.send('Weekdays application page');
+    response.render("index", {
+      application: {
+        title: 'Weekdays'
+      },
+      system: {
+        path: '/'
+      }
+    });
   })
 
   .listen(process.env.PORT || 5000);
